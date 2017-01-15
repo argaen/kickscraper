d3ca121f563428935968fc67fdb058f35fafc2f4
argaen <manu.mirandad@gmail.com>
2017-01-15 13:50:30 +0100

    Added travis config

--- setup.py
+++ setup.py
@@ -5,28 +5,21 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='kickscraper',
-    version='0.1.5',
+    version='0.1.6',
     description='A scraper for Crowdfunding platform projects like kickstarter',
     long_description=README_TEXT,
-
     url='https://github.com/argaen/kickscraper',
-
-    author='argaen',
+    author='Manuel Miranda',
     author_email="manu.mirandad@gmail.com",
     license='MIT',
-
     keywords=['kickstarter api crowdfunding projects'],
-
     packages=['kickscraper'],
-
-    install_requires=['requests>=2.9.1', 'beautifulsoup4>=4.4.1'],
-
+    install_requires=[
+        'requests>=2.9.1',
+        'beautifulsoup4>=4.4.1'
+    ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-
         'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2',
     ],
